211adb87041467e22cf2af1b2edc50e16439ea91
ZhaoJian <1349265742@qq.com>
2018-06-19 19:15:24 +0800

    还需要做navbar的状态提升

--- frontend/igame/src/App/Site/Safety/Changepwd.js
+++ frontend/igame/src/App/Site/Safety/Changepwd.js
@@ -1,21 +1,102 @@
 import React from 'react';
-import { List } from 'antd-mobile';
-import './Index.css';
 
+import { List, InputItem, Switch, Stepper, Range, Button,Toast } from 'antd-mobile';
+import { createForm } from 'rc-form';
+import { Models,session } from '../Models/Models'
 const Item = List.Item;
-const Brief = Item.Brief;
 
-export default class Changeped extends React.Component {
+class BasicInput extends React.Component {
+    // componentWillMount() {
+    //     console.log('000 Component WILL MOUNT!渲染前调用,在客户端也在服务端');
+    //     console.log(session.get_sid())
+    //     const sid = session.get_sid()
+    // }
+  state = {
+    value: 1,
+  }
+  onSubmit = () => {
+    this.props.form.validateFields({ force: true }, (error) => {
+      if (!error) {
+        console.log(this.props.form.getFieldsValue());
+        Toast.success("修改成功！",1);   //这里提交数据做处理
+        this.props.onChange()
+      } else {
+        alert('您的输入不完整！');
+      }
+    });
+  }
+  validateAccount = (rule, value, callback) => {
+    if (value && value.length > 4) {
+      callback();
+    } else {
+      callback(new Error('至少4个字符'));
+    }
+  }
+  render() {
+    const { getFieldProps, getFieldError } = this.props.form;
 
+    return (
+    <form>
+      <List
+        renderHeader={() => '您正在修改密码，请谨慎操作'}
+        renderFooter={() => getFieldError('account') && getFieldError('account')||getFieldError('password') && getFieldError('password').join(',')}
+      >
+        <InputItem
+          {...getFieldProps('account', {
+            // initialValue: 'little ant',
+            rules: [
+              { required: true, message: '输入不能为空' },
+              { validator: this.validateAccount },
+            ],
+          })}
+          clear
+          error={!!getFieldError('account')}
+          onErrorClick={() => {
+            alert(getFieldError('account').join('、'));
+          }}
+          placeholder="请输入旧的密码"
+          type='password'
+        >旧密码</InputItem>
 
-    render() {
-        return (
-            <div>
-                <List renderHeader={() => 'plese enter your password !'} className="my-list">
-                    <Item arrow="horizontal" onClick={()=>{}}>修改密码1111111</Item>
-                </List>
-            </div>
+        <InputItem {...getFieldProps('password',{
+            rules:[
+                { required: true, message: '输入不能为空' },
+                { validator: this.validateAccount },
+            ],
+        })} 
+        clear
+        error={!!getFieldError('password')}
+        onErrorClick={() => {
+            alert(getFieldError('password').join('、'));
+        }}
+        placeholder="请输入您的新密码" type="password"
+        
+        >新密码
+        </InputItem>
+        
+       
+        <Item>
+          <Button type="primary" size="small" inline onClick={this.onSubmit}>提交</Button>
+          <Button size="small" inline style={{ marginLeft: '2.5px' }} onClick={this.props.onChange}>取消</Button>
+        </Item>
+      </List>
+    </form>);
+  }
+}
+
+const BasicInputWrapper = createForm()(BasicInput);
+
+class Changepwd extends React.Component {
+    componentWillMount() {
+        console.log('000 Component WILL MOUNT!渲染前调用,在客户端也在服务端');
+        console.log(session.get_sid())
+        const sid = session.get_sid()
+    }
+    render (){
+        return(
+            this.sid?'':<BasicInputWrapper onChange={this.props.onChange} />
         );
     }
 }
-
+export default Changepwd;
+// ReactDOM.render(<BasicInputWrapper />, mountNode);
--- frontend/igame/src/App/Site/Safety/Index.js
+++ frontend/igame/src/App/Site/Safety/Index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { List } from 'antd-mobile';
 import './Index.css';
-import ChangeSafetyNav from './Changepwd';
+import Changepwd from './Changepwd';
+import Resetpwd from './Resetpwd';
+import { SiteLogin } from '../Common/Login';
+import { Models,session } from '../Models/Models';
 
 const Item = List.Item;
 const Brief = Item.Brief;
@@ -13,29 +16,62 @@ export default class Safety extends React.Component {
         SafetyNav:true,
         change:false,
         reset:false,
+        // sid:session.get_sid()
     }
 
-    changeSafetyNav = ()=>{
+    // componentWillMount() {
+    //     console.log('000 Component WILL MOUNT!渲染前调用,在客户端也在服务端');
+    //     console.log(session.get_sid())
+    //     this.setState({
+    //         sid:session.get_sid(),
+    //     });
+    // }
+
+    onCheckChange = ()=>{
+        if (session.get_sid()){
+            this.setState({
+                SafetyNav:!this.state.SafetyNav,
+                change:true,
+            });
+        }else{
+            const login = new SiteLogin( ()=>this.setState({SafetyNav:!this.state.SafetyNav,change:true}) );  
+            login.login();
+        }
+        
+    }
+    onCheckReset = ()=>{
         this.setState({
             SafetyNav:!this.state.SafetyNav,
-            change:true,
+            reset:true,
         });
     }
 
+    onChange = ()=>{
+        this.setState({
+            SafetyNav:!this.state.SafetyNav,
+            change:false,
+        });
+    }
     
+    onReset= ()=>{
+        this.setState({
+            SafetyNav:!this.state.SafetyNav,
+            reset:false,
+        });
+    }
     render() {
         return (
             <div>
                 {this.state.SafetyNav ? 
                     <List renderHeader={() => '请选择你要进行的操作'} className="my-list">
-                        <Item arrow="horizontal" onClick={this.changeSafetyNav}>修改密码</Item>
-                        <Item arrow="horizontal" onClick={() => {}}>重置密码（忘记密码）</Item>
+                        <Item arrow="horizontal" onClick={this.onCheckChange}>修改密码</Item>
+                        <Item arrow="horizontal" onClick={this.onCheckReset}>重置密码（忘记密码）</Item>
                     </List>
                 : 
                 ''
                 }
-                {this.state.change ? <ChangeSafetyNav />  : ''}
-                {this.state.reset ? <ChangeSafetyNav />  : ''}
+                {this.state.change ? <Changepwd onChange={this.onChange} />  : ''}
+                {this.state.reset ? <Resetpwd onReset={this.onReset} />  : ''}
             </div>
         );
     }
