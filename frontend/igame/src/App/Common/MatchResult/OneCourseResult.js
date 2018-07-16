1bf655e578465dab27ecdc0474a1a802a199b558
ZhaoJian <1349265742@qq.com>
2018-07-16 17:42:08 +0800

    成绩查询入口

---
+++ frontend/igame/src/App/Common/MatchResult/OneCourseResult.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavBar, WhiteSpace} from 'antd-mobile';
+import { Icon } from 'antd';
+
+export default class OneCourseResult extends React.Component{
+    render(){
+        return(
+            <div>
+                <NavBar
+                mode="light"
+                icon={<Icon type="left" />}
+                onLeftClick={()=>this.props.toMatchDetails()}    //返回轮次页
+                >{this.props.courseId[1]}成绩
+                </NavBar>
+                <WhiteSpace size='xl' />
+                <h1>赛事名称：{this.props.match.name}</h1>
+                <h1>赛事ID：{this.props.match.id}</h1>
+                <h1>轮次ID：{this.props.courseId[0]}</h1>
+                <button onClick={()=>this.props.showPage('OneCourseRanking')} >到排名</button>
+
+            </div>
+        );
+    }
+}
