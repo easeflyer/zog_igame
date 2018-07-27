19d0f068a7a64a198a64e042659d3b8d80d57d3c
Karmaliu <2134515368@qq.com>
2018-07-27 18:48:47 +0800

    桌号页面表格

--- frontend/igame/src/App/Common/MatchResult/OneBoard.js
+++ frontend/igame/src/App/Common/MatchResult/OneBoard.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { NavBar, WhiteSpace } from 'antd-mobile';
 import { Icon } from 'antd';
 import Game from '../../OdooRpc/Game';
+import OneBardTable from './OneBoardTable/OneBoardTable';
 
 export default class OneBoard extends React.Component {
+    state = {
+        boardId: this.props.boardId[0],
+        boardIds: this.props.boardId[1],
+        boardNumber: this.props.boardId[2],
+        data: []
+    }
     componentWillMount() {
         //***********接口方法调用**************
         /**
@@ -12,7 +19,12 @@ export default class OneBoard extends React.Component {
      */
 
         const m = new Game((data) => this.success(data), () => console.log('没有拿数据'));
-        m.round_deal_info(this.props.match.id, this.props.thisOneRound[0], this.props.boardId)
+        m.round_deal_info(this.props.match.id, this.props.thisOneRound[0], this.state.boardId)
+    }
+    success = (data) => {
+        this.setState({
+            data: data
+        })
     }
     render() {
         return (
@@ -21,8 +33,9 @@ export default class OneBoard extends React.Component {
                     mode="light"
                     icon={<Icon type="left" />}
                     onLeftClick={() => this.props.showPage('OneCourseResult')}    //到一轮结果
-                >第{this.props.boardId}副牌
+                >第{this.state.boardNumber}副牌
                 </NavBar>
+                <OneBardTable data={this.state.data} />
                 <WhiteSpace size='xl' />
                 <h1>赛事名称：{this.props.match.name}</h1>
                 <h1>赛事ID：{this.props.match.id}</h1>
