f06b8f7a3779ac17847df0a6b5b75c0a87db3eb2
ease <easenet@gmail.com>
2019-01-15 16:16:50 +0800

    修改bidblock 高度5->4

--- frontend/bridge_gui/config/deploy/fabfile.py
+++ frontend/bridge_gui/config/deploy/fabfile.py
@@ -5,22 +5,49 @@ env.port = '22'
 env.user = 's1'
 env.password = 's1'
 
-def hello():
-  print("hello world!")
+# fab hello:a=11,b=22
+def hello(a,b):
+  print("hello world!{}-{}".format(a,b))
 # 自动命令示例
-def list():
-  with cd('/home/s1/zog_igame/frontend/bridge_gui'):
+def list(dir="/home/s1/zog_igame/frontend/bridge_gui"):
+  with cd(dir):
     run('ls -l')
-    sudo('ps -aux')
     run('exit')
 
-# 自动部署示例
+# 自动部署
+def deploy(app='igame'):
+  eval("_"+app)()
+  run('exit')
 
 
-def deploy():
+def _itable():
+  with cd("/home/s1/gameLobby"):
+    #run('git reset --hard HEAD')
+    sudo('git checkout master')
+    sudo('git pull origin master')
+    #sudo('rm -rf build/')
+    sudo('yarn build')
+    # sudo('rm -rf /var/www/bridge_bui1/itable')
+    # sudo('cp -r dist/ /var/www/bridge_bui1/itable')
+
+
+# 计划修改为软连接版本。
+def _igame():
   with cd("/home/s1/zog_igame/frontend/bridge_gui"):
-    run('git reset --hard HEAD')
-    run('git pull origin bui-mobx-test1')
+    #run('git reset --hard HEAD')
+    sudo('git checkout bui-mogx-test1')
+    sudo('git pull origin bui-mobx-test1')
+    # sudo('rm -rf build/')
+    sudo('yarn build')
+    # sudo('rm -rf /var/www/bridge_bui1/igame')
+    # sudo('mv build/ /var/www/bridge_bui1/igame')
+
+
+# 正常工作的版本。删除重新拷贝
+def _igame1():
+  with cd("/home/s1/zog_igame/frontend/bridge_gui"):
+    #run('git reset --hard HEAD')
+    sudo('git pull origin bui-mobx-test1')
     sudo('rm -rf build/')
     sudo('yarn build')
     sudo('rm -rf /var/www/bridge_bui1/igame')
