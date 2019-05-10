e4bd40f5102169df7ebe98b6ec68e6a85ce9b9aa
ease <easenet@gmail.com>
2019-05-10 16:29:38 +0800

    fab 部署代码修改

--- frontend/bridge_gui/config/deploy/fabfile.1.py
+++ frontend/bridge_gui/config/deploy/fabfile.1.py
@@ -6,10 +6,10 @@ from fabric.api import *
 
 
 
-env.hosts = ['192.168.1.8']
+env.hosts = ['139.198.21.140']
 env.port = '22'
-env.user = 's1'
-env.password = 's1'
+env.user = 'ubuntu'
+env.password = 'Odooht163icp'
 
 # fab hello:a=11,b=22
 def hello(a,b):
@@ -27,21 +27,19 @@ def deploy(app='igame'):
 
 
 def _itable():
-  with cd("/home/s1/gameLobby"):
-    #run('git reset --hard HEAD')
-    #sudo('git fetch origin Qcloud')
-    sudo('git checkout Qcloud')
-    sudo('git pull origin Qcloud')
-    #sudo('rm -rf build/')
+  with cd("/opt/odoo/zog_igame/frontend/itable"):
+    sudo('git checkout develop')
+    sudo('git pull origin develop')
     sudo('yarn build')
-    # sudo('rm -rf /var/www/bridge_bui1/itable')
-    # sudo('cp -r dist/ /var/www/bridge_bui1/itable')
+def _imatch():
+  with cd("/opt/odoo/zog_igame/frontend/imatch"):
+    sudo('git checkout develop')
+    sudo('git pull origin develop')
+    sudo('yarn build')
 
 
-# 计划修改为软连接版本。
 def _igame():
-  with cd("/home/s1/zog_igame/frontend/bridge_gui"):
-    #run('git reset --hard HEAD')
+  with cd("/opt/odoo/zog_igame/frontend/itable"):
     sudo('git checkout test1')
     sudo('git pull origin test1')
     # sudo('rm -rf build/')
